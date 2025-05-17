2029e184cfcaffb8ad32ac6619c04d0867c28892
Peter Riek <Peter.Riek@gmx.de>
2025-05-17 19:50:18 +0200

    status endpoint no auth

--- apps/backend/src/main/java/me/peterrk/pan/backend/config/SecurityConfig.java
+++ apps/backend/src/main/java/me/peterrk/pan/backend/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**")
+        .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**", "/api/status")
             .permitAll()
             .anyRequest()
             .authenticated())
