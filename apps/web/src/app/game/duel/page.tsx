51e3bbfe84025b5bbf23ad0f362e79ccb0cdd109
Peter Riek <Peter.Riek@gmx.de>
2025-05-20 01:01:10 +0200

    fix detached head2

--- apps/web/src/app/game/duel/page.tsx
+++ apps/web/src/app/game/duel/page.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { auth } from "@/auth";
-import DuelGameView from "@/components/gameviews/duel-game";
+import DuelGameView from "@/components/game/multiplayer/duel-view";
 import useDuelSocket from "@/lib/hooks/use-duel-socket";
 import { Container, Typography } from "@mui/material";
 import React, { useEffect } from "react";
@@ -9,7 +9,7 @@ import React, { useEffect } from "react";
 const DuelPage = async () => {
   const session = await auth();
 
-  return <DuelGameView accessToken={session?.accessToken} roomId="room01"/>;
+  return <DuelGameView accessToken={session?.accessToken} roomId="room01" />;
 };
 
 export default DuelPage;
